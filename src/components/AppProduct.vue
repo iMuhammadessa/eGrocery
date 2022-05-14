2d5f080c13f4b8695abcc7d73f53e8f98088a3f9
Muhammad Essa <imuhammadessa@gmail.com>
2022-05-14 14:57:22 +0500

    app files

---
+++ src/components/AppProduct.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        bpm:1,
+        products: [
+            { img: 'pr1.png', title: 'Cabbage', sub },
+        ]
+    })
+}
+</script>
+
+<style>
+
+</style>
