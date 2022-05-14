f2dc50df599db0815d890039b264752403edcbf5
Muhammad Essa <imuhammadessa@gmail.com>
2022-05-14 23:04:42 +0500

    Home file completed

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -47,15 +47,66 @@
           <v-btn rounded color="green" dark class="px-8">See All</v-btn>
           </v-toolbar>
           <AppProduct />
+          <v-card flat color="#E2F2E5" class="rounded-xl mt-8">
+            <v-toolbar flat color="transparent" class="mb-4">
+              <v-toolbar-title class="text-h6 mt-5">Popular Bundle Pack</v-toolbar-title>
+            </v-toolbar>
+            <AppPack />
+          </v-card>
+          <v-card flat color="#FAFAFA" class="mt-8 py-5 px-16">
+            <div style="
+            position: absolute; margin-left: auto; margin-right: auto; left: 0; right: 0; text-align: center;">
+            <h3>What Our Client Say</h3>
+            </div>
+            <OurClient />
+          </v-card>
+          <v-toolbar flat color="transparent" class="mt-8">
+            <v-toolbar-title class="text-h6">
+              Our Trusted Partner
+            </v-toolbar-title>
+          </v-toolbar>
+          <AppPartner />
+          <v-card flat color="#E2F2E5" class="rounded-xl mtop mbottom mx-10 pa-4" height="350">
+            <v-row>
+              <v-col cols="12" xs="12" sm="12" md="5">
+                <v-card class="rounded-xl mt-n16 ml-16 mobile" outlined flat height="450" width="200">
+                  <v-img src="mobile.png"></v-img>
+                </v-card>
+              </v-col>
+              <v-col cols="12" xs="12" sm="6" md="7">
+                <h1 class="mt-16">Download App</h1>
+                <p>
+                  Download app from Play store or Apple store <br>
+                  to enjoy our best services all over Pakistan. <br>
+                  We deals with all types of grocery, food, packs & more. <br><br>
+                  70% OFF on first order!
+                </p>
+                <v-btn color="black" dark class="mb-10">
+                  <v-icon color="white" left>fab fa-apple</v-icon>
+                  App store
+                </v-btn>
+                <v-btn color="black" dark class="ml-4 mb-10">
+                  <v-icon color="white" left>fab fa-google-play</v-icon>
+                  Play store
+                </v-btn>
+              </v-col>
+            </v-row>
+          </v-card>
         </v-col>
       </v-row>
     </v-container>
+    <AppFooter />
   </v-app>
 </template>
 
 <script>
 import AppNavbar from '@/components/AppNavbar.vue'
 import AppCategory from '../components/AppCategory.vue'
+import AppProduct from '@/components/AppProduct.vue'
+import AppPack from '../components/AppPack.vue'
+import OurClient from '../components/OurClient.vue'
+import AppPartner from '@/components/AppPartner.vue'
+import AppFooter from '../components/AppFooter.vue'
 
   export default {
     name: 'HomeView',
@@ -64,7 +115,12 @@ import AppCategory from '../components/AppCategory.vue'
     }),
     components: {
     AppNavbar,
-    AppCategory
+    AppCategory,
+    AppProduct,
+    AppPack,
+    OurClient,
+    AppPartner,
+    AppFooter
 },
   }
 </script>
